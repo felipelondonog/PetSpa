b9af668b87ecc829383bf74743d4001a5193e1e2
felip <felipe12091@hotmail.com>
2023-05-30 12:24:30 -0500

    Nea class Charge.cs created

---
+++ PetSpaAPI/DAL/Entities/Charge.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetSpaAPI.DAL.Entities
+{
+    public class Charge
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Cargo")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Descripción")]
+        [MaxLength(200, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        public string? Description { get; set; }
+    }
+}
