2633d6b9157940eb55bfb1579db09a5541c8a34b
felip <felipe12091@hotmail.com>
2023-05-30 17:36:54 -0500

    New class Pet.cs created

--- PetSpaAPI/DAL/Entities/Breed.cs
+++ PetSpaAPI/DAL/Entities/Breed.cs
@@ -1,6 +1,21 @@
-﻿namespace PetSpaAPI.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetSpaAPI.DAL.Entities
 {
     public class Breed
     {
+        [Required]
+        public int Id { get; set; }
+
+        [Display(Name = "Raza")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Especie")]
+        public Species Species { get; set; }
+
+        [Display(Name = "Id Especie")]
+        public int SpeciesId { get; set; }
     }
 }
--- PetSpaAPI/DAL/Entities/Species.cs
+++ PetSpaAPI/DAL/Entities/Species.cs
@@ -1,6 +1,15 @@
-﻿namespace PetSpaAPI.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetSpaAPI.DAL.Entities
 {
     public class Species
     {
+        [Required]
+        public int Id { get; set; }
+
+        [Display(Name = "Especie")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; }
     }
 }
