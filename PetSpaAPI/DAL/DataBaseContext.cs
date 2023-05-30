e19b934cbf94cb6f14f67535562da8dbb2cb5f31
felip <felipe12091@hotmail.com>
2023-05-30 18:07:24 -0500

    Changes on appsetting.json and Program.cs

--- PetSpaAPI/DAL/DataBaseContext.cs
+++ PetSpaAPI/DAL/DataBaseContext.cs
@@ -1,6 +1,35 @@
-﻿namespace PetSpaAPI.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using PetSpaAPI.DAL.Entities;
+
+namespace PetSpaAPI.DAL
 {
-    public class DataBaseContext
+    public class DataBaseContext : DbContext
     {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Charge> Charges { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Species> Species { get; set; }
+        public DbSet<Breed> Breeds { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Charge>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Employee>().HasIndex(e => e.Cc).IsUnique();
+            modelBuilder.Entity<Service>().HasIndex(s => s.Name).IsUnique();
+            modelBuilder.Entity<Client>().HasIndex(c => c.Cc).IsUnique();
+            modelBuilder.Entity<Species>().HasIndex(s => s.Id).IsUnique();
+            modelBuilder.Entity<Breed>().HasIndex(b => b.Id).IsUnique();
+            modelBuilder.Entity<Pet>().HasIndex(p => p.Id).IsUnique();
+            modelBuilder.Entity<Appointment>().HasIndex(p => p.Id).IsUnique();
+        }
     }
 }
--- PetSpaAPI/Program.cs
+++ PetSpaAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using PetSpaAPI.DAL;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataBaseContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//builder.Services.AddTransient<SeederDb>();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
